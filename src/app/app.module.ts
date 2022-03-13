89893b9d1dabbf7102a0666bbfb5fc1bc37b438c
luc-dev10 <luc.dev1008@gmail.com>
2022-03-13 12:30:44 -0400

    add the reactive module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SignupComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
